e25c89fe7a5e664f10a3c58524e99710e2190880
Leon Breedt <ljb@bitserf.org>
2015-09-22 18:09:22 +1200

    Initial Commit

---
+++ OAuth2/OAuth2.h
@@ -0,0 +1,19 @@
+//
+//  OAuth2.h
+//  OAuth2
+//
+//  Created by Leon Breedt on 22/09/15.
+//  Copyright © 2015 Leon Breedt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OAuth2.
+FOUNDATION_EXPORT double OAuth2VersionNumber;
+
+//! Project version string for OAuth2.
+FOUNDATION_EXPORT const unsigned char OAuth2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OAuth2/PublicHeader.h>
+
+
